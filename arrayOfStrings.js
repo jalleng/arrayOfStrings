0806fa6cc0f804715f01bd9297abcaaf63d5d8c6
Jeff Gerber <jallengerber@gmail.com>
2016-09-20 17:53:52 -0700

    added function

---
+++ arrayOfStrings.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let cur = ['xxx', 'aaa', 'yyy', 'hij'];                                            //sample data
+let hist = [['aaa', 'bbb'], ['ccc', 'ddd', 'eee', 'fff'], ['ggg', 'hij', 'abc']];  //sample data
+
+let newStrings = [];
+let newHistory = [];
+
+let results = {};
+
+let historySearch = (current, history) => {
+  let seen = {};
+  for (let i = 0 ; i < history.length ; i++) {               //add old history to dictionary so it can be searched more efficiently.
+    for (let j = 0 ; j < history[i].length ; j++) {
+      seen[history[i][j]] = true;
+    }
+  }
+  for (let i = 0 ; i < current.length ; i++) {               //compare new string to dictionary keys.
+    if (!seen[current[i]]) {
+      newStrings.push(current[i]);
+    }
+  }
+  newHistory = history;
+  newHistory.shift();                                        //remove first element from history and add new element to the end.
+  newHistory.push(newStrings);
+
+  console.log(newStrings);
+  console.log(newHistory);
+  
+  results.newStrings = newStrings;                           //add the two values to a single object so that it can be returned by the function.
+  results.newHistory = newHistory;
+  
+  return results;
+};
+
+console.log(historySearch(cur, hist));                       //call function with sample data.
